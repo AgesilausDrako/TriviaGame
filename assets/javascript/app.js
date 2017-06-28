2cac0f0840a3d46415ae0eebe56c83b69e6ed894
AgesilausDrako <agesilaus@yahoo.com>
2017-06-28 18:42:29 -0400

    put in function to check arrays

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,95 +3,66 @@ $("document").ready(function(){
 	var correctGuesses = 0;
 	var incorrectGuesses = 0;
 	var unansweredGuesses = 0;
+	var $answers = "";
+
 	var correctArray = ["Grigori Rasputin", "Nicholas II", "Georgi Plekhanov",
 					"Alexander Solzhenitsyn", "Yuri Modin"];
 	var gameHtml = "";
 
-	
+		function gameResults () {
+			
+			$answers = $(":checked");
+			for (i=0; i<$answers.length; i++) {
+				console.log($answers[i].value, correctArray[i]);
+				if ($answers[i].value === correctArray[i]) {
+					correctGuesses++;
+				} else {
+					incorrectGuesses++;
+				}
+			}
 
-	function gameResults () {
+			var resultsHtml = "<h1 class='text-center'>Correct Answers: " + correctGuesses + "</h1>" +
+							"<h1 class='text-center'>Incorrect Answers: " + incorrectGuesses + "</h1>" +
+							"<h1 class='text-center'>Unanswered Questions: " + unansweredGuesses + "</h1>" +
+							"<img id='bear' src='assets/images/Bear.jpg' class='bear'>";
+		
 
-		// if ($(":checked").val() === "Grigori Rasputin") {
-		// 	correctGuesses++;
-		// } else if ($(":checked").val() != "Grigori Rasputin") {
-		// 	incorrectGuesses++;
-		// } 
-
-		if ($('input[type="radio"][name=monk]:checked').val() === "Grigori Rasputin") {
-			correctGuesses++;
-		} else {
-			incorrectGuesses++;
+			$("#game-box").html(resultsHtml);
 		}
+		//Counter for the timer
+		var counter = 12;
+		var interval;
 
 
-		if ($('input[type="radio"][name=tsar]:checked').val() === "Nicholas II") {
-			correctGuesses++;
-		} else {
-			incorrectGuesses++;
-		}
+		function run() {
+	      interval = setInterval(decrement, 1000);
+	    }
 
-		if ($('input[type="radio"][name=book]:checked').val() === "Georgi Plekhanov") {
-			correctGuesses++;
-		} else {
-			incorrectGuesses++;
-		}
+	    function decrement() {
+	    	counter--;
+	    	$("#counter").html(counter);
 
-		if ($('input[type="radio"][name=dissident]:checked').val() === "Alexander Solzhenitsyn") {
-			correctGuesses++;
-		} else {
-			incorrectGuesses++;
-		}
+	    	if (counter <= 20) {
+	    		$("#remaining").addClass("almostUp");
+	    	}
+	    	//  Once number hits zero...
+	      	if (counter === 0) {
 
-		if ($('input[type="radio"][name=agent]:checked').val() === "Yuri Modin") {
-			correctGuesses++;
-		} else {
-			incorrectGuesses++;
-		}
+	        //  ...run the stop function.
+	        stop();
 
-	
-		var resultsHtml = "<h1 class='text-center'>Correct Answers: " + correctGuesses + "</h1>" +
-						"<h1 class='text-center'>Incorrect Answers: " + incorrectGuesses + "</h1>" +
-						"<h1 class='text-center'>Unanswered Questions: " + unansweredGuesses + "</h1>" +
-						"<img id='bear' src='assets/images/Bear.jpg' class='bear'>";
-	
+	        gameResults();
 
-		$("#game-box").html(resultsHtml);
-	}
-	//Counter for the timer
-	var counter = 12;
-	var interval;
+	      }
+	    }
 
+	    //  The stop function
+	    function stop() {
 
-	function run() {
-      interval = setInterval(decrement, 1000);
-    }
+	      clearInterval(interval);
+	      $("#remaining").removeClass("almostUp");
+	    }
 
-    function decrement() {
-    	counter--;
-    	$("#counter").html(counter);
-
-    	if (counter <= 20) {
-    		$("#remaining").addClass("almostUp");
-    	}
-    	//  Once number hits zero...
-      	if (counter === 0) {
-
-        //  ...run the stop function.
-        stop();
-
-        gameResults();
-
-      }
-    }
-
-    //  The stop function
-    function stop() {
-
-      clearInterval(interval);
-      $("#remaining").removeClass("almostUp");
-    }
-
-    run();
-
+	    run();
 
 });
